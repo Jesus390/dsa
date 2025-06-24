bcf1cc04dad344afc9132310206d0c49c67a9e36
Jesus390 <jesusespinola90@gmail.com>
2025-06-23 23:59:07 -0300

    add - algoritmo dfs(Depth First Search)

--- graphs/graphs.py
+++ graphs/graphs.py
@@ -1,36 +1,60 @@
 class Graph:
     def __init__(self, size):
-        self.adj_matriz = [[None]*size for _ in range(size)]
+        self.adj_matrix = [[0] * size for _ in range(size)]
         self.size = size
-        self.nombre_nodos = [''] * size
+        self.vertex_data = [''] * size  
 
-    def agregar_nodo(self, nombre, posicion):
-        self.nombre_nodos[posicion] = nombre
-    
-    def agregar_arista(self, origen, destino, peso):
-        self.adj_matriz[origen][destino] = peso
-    
+    def add_edge(self, u, v):
+        if 0 <= u < self.size and 0 <= v < self.size:
+            self.adj_matrix[u][v] = 1
+            self.adj_matrix[v][u] = 1
+
+    def add_vertex_data(self, vertex, data):
+        if 0 <= vertex < self.size:
+            self.vertex_data[vertex] = data
+
     def print_graph(self):
+        print("Adjacency Matrix:")
+        for row in self.adj_matrix:
+            print(' '.join(map(str, row)))
+        print("\nVertex Data:")
+        for vertex, data in enumerate(self.vertex_data):
+            print(f"Vertex {vertex}: {data}")
+            
+    def dfs_util(self, v, visited):
+        visited[v] = True
+        print(self.vertex_data[v], end=' ')
+
         for i in range(self.size):
-            for j in range(self.size):
-                if self.adj_matriz[i][j] is not None:
-                    print(f"{self.nombre_nodos[i]}:{self.nombre_nodos[j]}={self.adj_matriz[i][j]}", end=' ')
+            if self.adj_matrix[v][i] == 1 and not visited[i]:
+                self.dfs_util(i, visited)
 
-    def print_adj_matriz(self):
-        for i in range(self.size):
-            print(self.adj_matriz[i])
+    def dfs(self, start_vertex_data):
+        visited = [False] * self.size
+        start_vertex = self.vertex_data.index(start_vertex_data)
+        self.dfs_util(start_vertex, visited)
 
+g = Graph(7)
 
-graph = Graph(4)
-graph.agregar_nodo('A', 0)
-graph.agregar_nodo('B', 1)
-graph.agregar_nodo('C', 2)
-graph.agregar_nodo('D', 3)
-graph.agregar_arista(0, 1, 2)
-graph.agregar_arista(1, 2, 3)
-graph.agregar_arista(2, 3, 1)
-graph.agregar_arista(0, 2, 4)
-graph.agregar_arista(3, 3, 10)
-graph.print_graph()
-print()
-graph.print_adj_matriz()
+g.add_vertex_data(0, 'A')
+g.add_vertex_data(1, 'B')
+g.add_vertex_data(2, 'C')
+g.add_vertex_data(3, 'D')
+g.add_vertex_data(4, 'E')
+g.add_vertex_data(5, 'F')
+g.add_vertex_data(6, 'G')
+
+g.add_edge(3, 0)  # D - A
+g.add_edge(0, 2)  # A - C
+g.add_edge(0, 3)  # A - D
+g.add_edge(0, 4)  # A - E
+g.add_edge(4, 2)  # E - C
+g.add_edge(2, 5)  # C - F
+g.add_edge(2, 1)  # C - B
+g.add_edge(2, 6)  # C - G
+g.add_edge(1, 5)  # B - F
+
+g.print_graph()
+
+print("\nDepth First Search starting from vertex D:")
+g.dfs('D')
